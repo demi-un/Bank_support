cf709d7f26b4f6a38e9d2d29be57d3fdb062613d
Guzhov Roman <guzovvrom277@gmail.com>
2025-12-06 13:33:43 +0500

    add sistem files

--- bot/venv/bin/activate.fish
+++ bot/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/Users/romanguzov/PythonProjects/Хакатон/bot/venv'
+set -gx VIRTUAL_ENV /Users/romanguzov/PythonProjects/Bank_support/bot/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
