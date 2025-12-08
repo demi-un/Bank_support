a7644b5cb6bc429fe8c50fc3e692849dc1649070
Guzhov Roman <guzovvrom277@gmail.com>
2025-12-08 20:24:30 +0500

    добавлен перенос данных из data.json в базу знаний, семантический поиск работает плохо

--- chromadb/main.py
+++ chromadb/main.py
@@ -2,6 +2,12 @@ import chromadb
 from sentence_transformers import SentenceTransformer
 import json
 
+
+# data - словарь "вопрос": "ответ"
+with open("data.json") as f:
+    data = json.load(f) 
+
+
 # инициализация векторизатора
 model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
@@ -11,9 +17,6 @@ collection = client.get_or_create_collection(
     name="faq",
     metadata={"hnsw:space": "cosine"}  # метрика расстояния
 )
-
-# данные
-data = {}
 
 # формируем списки
 texts = list(data.keys())
@@ -30,12 +33,28 @@ collection.add(
 
 
 # поиск в db
-query = "Что делать если потерял карту?"
+query = "Не отправляются документы через чат поддержки."
 query_emb = model.encode([query]).tolist()
 
 result = collection.query(
     query_embeddings=query_emb,
-    n_results=1
+    n_results=3
 )
 
-print(result)
+
+# print(result)
+
+questions = result['documents'][0]
+results = []
+for i in result['metadatas'][0]:
+    results.append(i['answer'])
+
+
+print(f"ИСХОДНЫЙ ВОПРОС: {query}\n")
+
+for i in range(len(questions)):
+    print(f"ВОПРОС {i}: ")
+    print(questions[i])
+    print(f"ОТВЕТ {i}: ")
+    print(results[i])
+    print("")
