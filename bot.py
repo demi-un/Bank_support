f8e5a3aa2024ec4abb89125a7e128d663ecc7e74
Guzhov Roman <guzovvrom277@gmail.com>
2025-12-10 13:06:41 +0500

    add /start

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import telebot
 from langchain.schema import HumanMessage, SystemMessage
 from langchain_community.chat_models.gigachat import GigaChat
 
-# -----------------------------------------------------------------------------
 # получение API_KEY и TOKEN из .env
 API_KEY = ""
 TOKEN = ""
@@ -17,11 +16,9 @@ with open(".env") as f:
         if line.startswith("TOKEN="):
             TOKEN = line[len("TOKEN="):].strip()
 
-# -----------------------------------------------------------------------------
-# импорт семантического поиска (если используешь)
+# импорт семантического поиска
 from database import dbsearch
 
-# -----------------------------------------------------------------------------
 # LLM
 giga = GigaChat(
     credentials=API_KEY,
@@ -52,34 +49,47 @@ SYSTEM_PROMPT = [
 """)
 ]
 
-# -----------------------------------------------------------------------------
 # история пользователей
 users_history = {}
+
 
 # функция вызова LLM
 def llm(user_message, user_history):
     # поиск в базе по вопросу пользователя
     db_results = dbsearch(user_message)
 
-    user_history.append(HumanMessage(content=user_message + f"; СИСТЕМНОЕ СООБЩЕНИЕ: Контекст из базы данных (используй его, если он помогает ответить на заданный вопрос):\n{db_results}"))
+    user_history.append(HumanMessage(
+        content=user_message + f"; СИСТЕМНОЕ СООБЩЕНИЕ: Контекст из базы данных (используй его, если он помогает ответить на заданный вопрос):\n{db_results}"))
 
     gigachat_answer = giga.invoke(user_history)
     user_history.append(gigachat_answer)
 
     return gigachat_answer.content, user_history
 
-# -----------------------------------------------------------------------------
+
 # инициализация бота
 bot = telebot.TeleBot(TOKEN)
 
-# -----------------------------------------------------------------------------
+
 # обработка всех не текстовых сообщений
 @bot.message_handler(content_types=['audio', 'video', 'document', 'photo', 'sticker', 'voice', 'location', 'contact'])
 def not_text(user_message):
     user_id = user_message.chat.id
     bot.send_message(user_id, "Я работаю только с текстовыми сообщениями")
 
-# -----------------------------------------------------------------------------
+
+# /start
+start_message = """
+Стартовое сообщение
+"""
+
+
+@bot.message_handler(commands=['start'])
+def start(user_message):
+    user_id = user_message.chat.id
+    bot.send_message(user_id, start_message)
+
+
 # обработка текстовых сообщений
 @bot.message_handler(content_types=['text'])
 def handle_text_message(user_message):
@@ -100,7 +110,7 @@ def handle_text_message(user_message):
     # отправляем ответ пользователю
     bot.send_message(user_id, gigachat_answer)
 
-# -----------------------------------------------------------------------------
+
 # запуск бота
 if __name__ == "__main__":
     print("Бот запущен...")
